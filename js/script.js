660a90be7b25f45e68f89d837d36d52c1a203f06
jheikoff <60203909+jheikoff@users.noreply.github.com>
2021-03-17 22:37:30 -0600

    invalid chars

--- js/script.js
+++ js/script.js
@@ -2,20 +2,35 @@ function validateForm(){
     var pw = document.querySelector("#txtSPassword");
     var pw2 = document.querySelector("#txtSPasswordMatch");
     var divPWError = document.querySelector("#divPasswordError");
+    var formIsValid = true;
     if(pw !== pw2){
         /* Show Error and Change Style of confirmpassword field */
         divPWError.classList.remove("invisible");
         divPWError.innerHTML = "Passwords do not match.";
         pw2.classList.add("hasError");
         /* Reject */
-        return false;
+        formIsValid = false;
     }
     else{
         divPWError.classList.add("invisible");
         divPWError.innerHTML = "";
-        pw2.classList.remove("hasError");
-        return true;    
+        pw2.classList.remove("hasError");  
     }
+
+    var elements = document.getElementsByTagName("input");
+    var invalidChars = ['<','>','#','-','{','}','(',')','\'','"','`'];
+    for(let i = 0; i < elements.length; i++){
+        for(let j = 0; j < invalidChars.length; j++){
+            if(elements[i].value.indexOf(invalidChars[j]) != -1){
+                elements[i].classList.add("hasError");
+                formIsValid = false;
+            }
+        }
+    }
+
+
+
+    return formIsValid;
 
     
 }
