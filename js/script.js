a767dc33c7d337590cc19a6a5857db4346e29a48
jheikoff <60203909+jheikoff@users.noreply.github.com>
2021-03-17 21:33:20 -0600

    Error

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 function validateForm(){
     var pw = document.querySelector("#txtSPassword");
     var pw2 = document.querySelector("#txtSPasswordMatch");
-    if(pw != pw2){
-        alert("Passwords do not match")
-        /** Show Error and Change Style of confirmpassword field */
+    var divPWError = document.querySelector("#divPasswordError");
+    if(pw !== pw2){
+        /* Show Error and Change Style of confirmpassword field */
+        divPWError.classList.remove("invisible");
+        divPWError.innerHTML = "Passwords do not match.";
+        pw2.classList.add("hasError");
+        /* Reject */
         return false;
     }
+    else{
+        divPWError.classList.add("invisible");
+        divPWError.innerHTML = "";
+        pw2.classList.remove("hasError");
+        return true;    
+    }
 
-    return false;
+    
 }
